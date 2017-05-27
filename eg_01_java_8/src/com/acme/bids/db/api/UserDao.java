52e83ee8938cb492a56c3f48742944a4cba28a7c
Michael Easter <codetojoy@gmail.com>
2017-05-27 14:13:44 -0300

    incremental change as part of a larger frenzy of activity

--- eg_01_java_8/src/com/acme/bids/db/api/UserDao.java
+++ eg_01_java_8/src/com/acme/bids/db/api/UserDao.java
@@ -3,6 +3,7 @@ package com.acme.bids.db.api;
 
 import com.acme.bids.db.impl.UserDaoImpl;
 
+// for simplicity, we use a facade class instead of an interface
 public class UserDao {
     private UserDaoImpl userDaoImpl = new UserDaoImpl();
 
--- eg_01_java_8/src/com/acme/bids/service/api/UserService.java
+++ eg_01_java_8/src/com/acme/bids/service/api/UserService.java
@@ -3,6 +3,7 @@ package com.acme.bids.service.api;
 
 import com.acme.bids.service.impl.UserServiceImpl;
 
+// for simplicity, we use a facade class instead of an interface
 public class UserService {
     private UserServiceImpl userServiceImpl = new UserServiceImpl();
 
